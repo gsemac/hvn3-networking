06f6452c6e2ff25c074ed98e61dbbb7570b49b53
gabe.semac@uleth.ca <gabe.semac@uleth.ca>
2017-08-11 01:33:14 -0600

    Added UdpConnection class

---
+++ cpp-networking/include/net/UdpConnection.h
@@ -0,0 +1,19 @@
+#include "net/Socket.h"
+
+namespace hvn3 {
+	namespace Net {
+		namespace Sockets {
+
+			class UdpConnection {
+
+			public:
+
+			private:
+				float _timeout;
+				float _timeout_accumulator;
+
+			};
+
+		}
+	}
+}
